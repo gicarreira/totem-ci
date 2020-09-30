9d89561b27a63c2f51b87e4a59795a95fb0d6225
gicarreira <59888584+gicarreira@users.noreply.github.com>
2020-09-30 16:02:07 -0300

    Update index.js
    atualização de formulario

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ class Input extends React.Component {
     super();
     this.state = {
       nome: "",
-      celular: ""
+      celular: "",
+      documento: ""
     };
     this.onChange = (evento) => {
     const state = Object.assign({}, this.state);
@@ -17,12 +18,25 @@ class Input extends React.Component {
     state[campo] = evento.target.value;
     this.setState(state); 
     };
+    this.onSubmit = (evento) => {
+        evento.preventDefault();
+        console.log(this.state);
+    };
   }
+  
   render() {
     return (
     <div>
         Nome: <input name="nome" value={this.state.nome} onChange={this.onChange} type="text" /><br />
         Celular: <input name="celular" value={this.state.celular} onChange={this.onChange} type="text" /><br />
+        Documento: <select name="documento" onChange={this.onChange} value={this.state.documento}>
+                    <option value="">Selecione</option>
+                    <option value="1">RNE</option>
+                    <option value="2">CPF</option>
+                    <option value="3">PROTOC</option>
+                  </select>
+        Numero: <input name="numero" value={this.state.numero} onChange={this.onChange} type="text" /><br />
+        <button onClick={this.onSubmit}>Enviar</button>
     </div>
     );
   }
